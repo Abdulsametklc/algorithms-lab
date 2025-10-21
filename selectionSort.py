3430ef2f66fe03ee8a646eaa71596526dfa1345f
Abdulsamet KILIÇ <96330083+Abdulsametklc@users.noreply.github.com>
2025-10-21 09:26:44 +0300

    Add selection sort implementation in selectionSort.py
    Implement selection sort algorithm to sort an array.

---
+++ selectionSort.py
@@ -0,0 +1,12 @@
+array = [64, 34, 25, 5, 22, 11, 90, 12]
+
+n = len(array)
+
+for i in range(n-1):
+  min_index = i
+  for j range(i+1, n):
+    if array[j] < array[min_index]:
+      min_index = j
+  min_value = array.pop(min_index)
+  array.insert(i, min_value)
+array
